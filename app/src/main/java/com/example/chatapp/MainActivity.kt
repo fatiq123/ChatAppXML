f777bde7fe88ea2d3db739b35fd4272deccf566e
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-05 06:18:30 -0700

    authentication and account creation done for user

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.chatapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +17,10 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.signInButton.setOnClickListener {
-
+            signIn()
         }
         binding.signUpButton.setOnClickListener {
-
+            createAccount()
         }
 
         binding.textViewRegister.setOnClickListener {
@@ -31,5 +33,84 @@ class MainActivity : AppCompatActivity() {
             binding.flipper.setOutAnimation(this, R.anim.slide_out_left)
             binding.flipper.showPrevious()
         }
+    }
+
+    private fun signIn() {
+
+        val email = binding.singInInputEmail.editText?.text.toString().trim()
+        val password = binding.singInInputPassword.editText?.text.toString().trim()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(
+                this,
+                "You should provide an email and a password",
+                Toast.LENGTH_LONG
+            ).show()
+
+            return
+        }
+
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this,
+                        "User signed in",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Couldn't sign in/nSomething went wrong !",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
+    private fun createAccount() {
+
+        val email = binding.singUpInputEmail.text.toString().trim()
+        val password = binding.singUpInputPassword.text.toString().trim()
+        val confirmPassword = binding.singUpInputConfirmPassword.text.toString().trim()
+
+        if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            Toast.makeText(
+                this,
+                "You should provide an email and a password",
+                Toast.LENGTH_LONG
+            ).show()
+
+            return
+        }
+
+        if (password != confirmPassword) {
+            Toast.makeText(
+                this,
+                "Passwords don't match",
+                Toast.LENGTH_LONG
+            ).show()
+
+            return
+        }
+
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this,
+                        "Account created",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Account wasn't created",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 }
