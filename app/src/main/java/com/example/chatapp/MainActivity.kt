7f7a1304d2638fcbee86346872d11d51a2ddcaa6
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-05 06:49:28 -0700

    getImage function done using ActivityResultLauncher same as I used in Weather App

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import com.example.chatapp.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -10,6 +13,7 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var getResult: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +40,19 @@ class MainActivity : AppCompatActivity() {
         binding.textViewSignUp.setOnClickListener {
             startPreviousAnimation()
         }
+
+        // uploading the profile image
+        binding.profileImage.setOnClickListener {
+            uploadImage()
+        }
+
+        getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                binding.profileImage.setImageURI(it.data?.data)
+            }
+        }
     }
+
 
     private fun signIn() {
 
@@ -127,5 +143,11 @@ class MainActivity : AppCompatActivity() {
         binding.flipper.setInAnimation(this, R.anim.slide_in_right)
         binding.flipper.setOutAnimation(this, R.anim.slide_out_left)
         binding.flipper.showPrevious()
+    }
+
+    private fun uploadImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        getResult.launch(intent)
     }
 }
